510884012df9e82909c67917c81bf23a583757d8
Philip R McDavid <31264591+FftyShadesofCode@users.noreply.github.com>
2022-04-28 21:39:45 -0400

    Navbar not showing up

---
+++ frameoflightdesigns/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Navbar = () => {
+    return (
+        <>
+            <nav className='flex items-center flex-wrap p-3 bg-gradient-to-t from-folpd-reds-dk to-folpd-reds-ruby'>
+
+            </nav>
+        </>
+    );
+};
